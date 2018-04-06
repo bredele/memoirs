a5318a7eb6c2e767f19bd2f82bd4a2e189515110
bredele <olivier.wietrich@gmail.com>
2018-04-06 11:01:30 -0600

    Works with date string formats

--- test/memoirs.js
+++ test/memoirs.js
@@ -25,3 +25,13 @@ test('sort dates and convert in ms', assert => {
   ])
   assert.deepEqual(sorted, [946710000000, 4102470000000, 10413817200000])
 })
+
+test('sort work with date string formats', assert => {
+  assert.plan(1)
+  const sorted = history([
+    new Date('01-01-2100'),
+    'Jan, 01 2000',
+    new Date('01-01-2300'),
+  ])
+  assert.deepEqual(sorted, [946710000000, 4102470000000, 10413817200000])
+})
