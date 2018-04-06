40cd567f165cfb1957523e4cd2e04498847a723a
bredele <olivier.wietrich@gmail.com>
2018-04-06 11:00:12 -0600

    Convert dates in ms

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+module.exports = (arr) => {
+  return arr.map(parse).sort((before, after) => {
+    return before - after
+  })
+}
+
+
+/**
+ * Parse given time into a date in ms.
+ *
+ * @param {Date|Number|String} time
+ * @return {Number}
+ * @api private
+ */
+
+function parse (time) {
+  return typeof time === 'number' ? time : Date.parse(time)
+}
--- test/memoirs.js
+++ test/memoirs.js
@@ -5,12 +5,23 @@
 const test = require('tape')
 const history = require('..')
 
-test('sort dates and convert in ms', assert => {
+test('sort dates', assert => {
   assert.plan(1)
   const sorted = history([
     new Date('01-01-2100').getTime(),
     new Date('01-01-2000').getTime(),
     new Date('01-01-2300').getTime(),
+  ])
+  assert.deepEqual(sorted, [946710000000, 4102470000000, 10413817200000])
+})
+
+
+test('sort dates and convert in ms', assert => {
+  assert.plan(1)
+  const sorted = history([
+    new Date('01-01-2100'),
+    new Date('01-01-2000'),
+    new Date('01-01-2300'),
   ])
   assert.deepEqual(sorted, [946710000000, 4102470000000, 10413817200000])
 })
