720d6f809a8630d3f07e7c4e391bc7cf8cf1e04a
bredele <olivier.wietrich@gmail.com>
2018-04-06 11:27:16 -0600

    Update tests to make it work on different timezones

--- test/memoirs.js
+++ test/memoirs.js
@@ -7,46 +7,54 @@ const history = require('..')
 
 test('sort dates', assert => {
   assert.plan(1)
-  const sorted = history([
+  const dates = [
     new Date('01-01-2100').getTime(),
     new Date('01-01-2000').getTime(),
-    new Date('01-01-2300').getTime(),
-  ])
-  assert.deepEqual(sorted, [946710000000, 4102470000000, 10413817200000])
+    new Date('01-01-2300').getTime()
+  ]
+  const sorted = history(dates)
+  assert.deepEqual(sorted, [dates[1], dates[0], dates[2]])
 })
 
 
 test('sort dates and convert in ms', assert => {
   assert.plan(1)
-  const sorted = history([
+  const dates = [
     new Date('01-01-2100'),
     new Date('01-01-2000'),
-    new Date('01-01-2300'),
-  ])
-  assert.deepEqual(sorted, [946710000000, 4102470000000, 10413817200000])
+    new Date('01-01-2300')
+  ]
+  const sorted = history(dates)
+  assert.deepEqual(sorted, [dates[1].getTime(), dates[0].getTime(), dates[2].getTime()])
 })
 
 test('sort work with date string formats', assert => {
   assert.plan(1)
-  const sorted = history([
+  const dates = [
     new Date('01-01-2100'),
     'Jan, 01 2000',
     new Date('01-01-2300'),
-  ])
-  assert.deepEqual(sorted, [946710000000, 4102470000000, 10413817200000])
+  ]
+  const sorted = history(dates)
+  assert.deepEqual(sorted, [new Date(dates[1]).getTime(), dates[0].getTime(), dates[2].getTime()])
 })
 
 
 test('should sort object containing dates with a given property name', assert => {
   assert.plan(1)
+  const dates = [
+    new Date('01-01-2100'),
+    new Date('01-01-2000'),
+    new Date('01-01-2300')
+  ]
   const sorted = history([
-    {expiry: new Date('01-01-2100')},
-    {expiry: new Date('01-01-2000')},
-    {expiry: new Date('01-01-2300')}
+    {expiry: dates[0]},
+    {expiry: dates[1]},
+    {expiry: dates[2]}
   ], 'expiry')
   assert.deepEqual(sorted, [
-    {expiry: 946710000000},
-    {expiry: 4102470000000},
-    {expiry: 10413817200000}
+    {expiry: dates[1].getTime()},
+    {expiry: dates[0].getTime()},
+    {expiry: dates[2].getTime()}
   ])
 })
